4050970020c710f6e5eb71f85f3033ab4587f4b9
ponceae <ponceae@dukes.jmu.edu>
2022-02-01 01:10:22 -0500

    out of order cluster found, unable to write it

--- p1_example/project1.c
+++ p1_example/project1.c
@@ -20,10 +20,13 @@ struct entry {
 	char fn[12];
 	int cluster;
 };
- 
+
+struct flag {
+	bool order;
+};
 /*
  * Driver that takes an input file and a map file and extracts its
- * contents to aid in data recovery. (DESCRIBE BETTER)
+ * contents to aid in data recovery based on clusters from the map.
  */
 int main (int argc, char **argv) {
 	
@@ -60,22 +63,39 @@ int main (int argc, char **argv) {
 		map = fopen(mapfn, "r");
 	}
 
+	// for reading in each line
 	struct entry entry;
+	
 	char buffer[CLUSTER_SIZE];
+	
+	// count aids in calculating offset
 	int count = 0;
+	
+	// file descriptors to check if a file has already been open
 	int jfd = -1;
 	int hfd = -1;
+	
+	// these must be initalized outside the loop to prevent seg faults
 	FILE *jpg = NULL;
 	FILE *htm = NULL;
 	
+	// keep track of order of clusters, if out of order set flag
+	int dupe_clus = -1;
+	struct flag flag;
+	
+	// read each line until EOF
 	while (fread(&entry, sizeof(entry), 1, map) == 1) {
+		
+		// appends a newline character to the filename for opening
 		char filename[12];
 		strncpy(filename, entry.fn, 12);
 		filename[12] = '\0';
 		
+		// file is .jpg 
 		if (strstr(filename, ".jpg")) {
-			// open file X, creating it if it does not already exist
+			// if not -1, file has not been created
 			if (jfd == -1) {
+				// open file X, creating it if it does not already exist
 				jpg = fopen(filename, "a+");
 				jfd = fileno(jpg);
 			}
@@ -91,23 +111,40 @@ int main (int argc, char **argv) {
 		} 
 		
 		if (strstr(filename, ".htm")) {
+			// if not -1, file has not been created
 			if (hfd == -1) {
+				// open file X, creating it if it does not already exist
 				htm = fopen(filename, "a+");
 				hfd = fileno(htm);
 			}
-			// open file X, creating it if it does not already exist
-			FILE *htm = fopen(filename, "a+");
-			// in file X, seek to position Y*CLUSTER_SIZE
-			fseek(htm, count * CLUSTER_SIZE, SEEK_SET);
-			// in the input file, seek to position i*CLUSTER_SIZE
-			fseek(input, count * CLUSTER_SIZE, SEEK_SET);
-			//read CLUSTER_SIZE bytes from input file
-			fread(&buffer, sizeof(buffer), 1, input);
-			// write them to file X			
-			fwrite(&buffer, sizeof(buffer), 1, htm);
-			count++;	
+			
+			// determines if a cluster is in order
+			if (dupe_clus <= entry.cluster) {
+				// in file X, seek to position Y*CLUSTER_SIZE
+				fseek(htm, count * CLUSTER_SIZE, SEEK_SET);
+				// in the input file, seek to position i*CLUSTER_SIZE
+				fseek(input, count * CLUSTER_SIZE, SEEK_SET);
+				//read CLUSTER_SIZE bytes from input file
+				fread(&buffer, sizeof(buffer), 1, input);
+				// write them to file X			
+				fwrite(&buffer, sizeof(buffer), 1, htm);
+				count++;
+				printf("%d\n", dupe_clus);
+				dupe_clus++;
+			} else {
+				flag.order = false;
+			}
 		}
 	}
+	
+	// write the out of order cluster at the EOF 
+	if (flag.order == false) {
+		// THIS BRANCH GETS REACHED BUT DOES NOT MANIPULATE THE HTM FILE AT ALL
+		printf("BRANCH REACHED: CLUSTER -> %d\n", dupe_clus);
+		
+	}
+	
+	// resource management
 	fclose(jpg);
 	fclose(htm);
 	fclose(input);
